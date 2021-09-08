605afdcf7a188e78e55454fb8872f6193ffc90e5
xloris <lorisginer34@gmail.com>
2021-09-08 13:45:47 +0200

    test upload

--- src/stories/Header.tsx
+++ src/stories/Header.tsx
@@ -34,11 +34,11 @@ export const Header: React.FC<HeaderProps> = ({ user, onLogin, onLogout, onCreat
       </div>
       <div>
         {user ? (
-          <Button size="small" onClick={onLogout} label="Log out" />
+          <Button size="small"  label="Log out" />
         ) : (
           <>
-            <Button size="small" onClick={onLogin} label="Log in" />
-            <Button primary size="small" onClick={onCreateAccount} label="Sign up" />
+            <Button size="small" label="Log in" />
+            <Button primary size="small" label="Sign up" />
           </>
         )}
       </div>

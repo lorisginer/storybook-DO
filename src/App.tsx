7bae542d90456f6c2cfb84da3eac96b9206f4250
xloris <lorisginer34@gmail.com>
2021-09-08 12:28:47 +0200

    fix error fontawesome

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 
 library.add(fab, faCheckSquare, faCoffee)
 

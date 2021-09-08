673fcd0302087c5a84ff5c8df766907a42266a46
xloris <lorisginer34@gmail.com>
2021-09-08 17:14:07 +0200

    fix package.json problem

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,9 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <h1>salut eliott</h1>,
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
